3cce8f22e21ac1d49db09330aa1a2d02f8bb44e3
SureshPasupuleti <sureshpasupuleti@sureshpuletimbp.gap.com.gap.com>
2018-01-17 16:21:44 -0800

    es-000 [suresh] initilal project commit

--- src/main/java/com/spring/kafka/receiver/EMailService.java
+++ src/main/java/com/spring/kafka/receiver/EMailService.java
@@ -3,4 +3,5 @@ package com.spring.kafka.receiver;
 
 public interface EMailService {
 public boolean sendSMTPMail(String toAddress, String subject, String message);
+public boolean getStatus(String toAddress);
 }
